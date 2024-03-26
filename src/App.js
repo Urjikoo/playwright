3705859ee237be224c1cc0efa156c87fdecb20e9
urji h <urjig122@gmail.com>
2024-03-26 09:32:21 -0400

    last test

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import TestUser from '../src/TestUser.js'
+
+function App() {
+  return (
+    <div className="App">
+      <TestUser/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/TestUser.js
@@ -0,0 +1,27 @@
+import { useRef, useState, useEffect} from "react";
+
+
+
+const TestUser = ()=>{
+  const [submit,setSubmit]= useState("Sign up")
+  return(
+    <div>
+    <head>
+  <title>playwright test</title>
+</head>
+    <h1 data-testid="cypress-h1">Testing log in page</h1>
+    <div>
+    <span>name</span>
+    <input type='text'/>
+    <span>email</span>
+    <input type='email'/>
+    <span>password</span>
+    <input type='text'/>
+    </div>
+    <div className="sumbit">
+      <button>Submit</button>
+    </div>
+    </div>
+  )
+}
+export default TestUser
